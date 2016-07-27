5f454ee5b51b4f2232990156c7b45f2a99c70a37
Ahmed Almazidi <a.almazidi92@gmail.com>
2016-07-27 13:22:01 +0000

    Made validation of uniqueness of email case insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,7 @@ describe User do
 
   context "validations" do
     it { should validate_presence_of :email }
-    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of :name }
     it { should validate_presence_of :password }
     it { should validate_confirmation_of :password }
